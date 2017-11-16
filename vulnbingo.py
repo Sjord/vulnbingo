205d5303dd4502aa48cbb06f33c9901f4118a700
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2017-11-16 17:03:46 +0100

    Create bingo card with vulnerabilities

---
+++ vulnbingo.py
@@ -0,0 +1,25 @@
+from docx import Document
+import random
+
+vulns = []
+with open('vulns.txt') as fp:
+    for line in fp:
+        parts = line.split(',', 1)
+        vulns.append((parts[0].strip(), parts[1].strip()))
+
+rand = random.SystemRandom()
+rand.shuffle(vulns)
+vuln_id = 0
+
+doc = Document("template.docx")
+for table in doc.tables:
+    for cell in table._cells:
+        title, description = vulns[vuln_id]
+        vuln_id += 1
+        for para in cell.paragraphs:
+            if para.text == "Vuln":
+                para.text = title
+            elif para.text == "description":
+                para.text = description
+
+doc.save("output.docx")
